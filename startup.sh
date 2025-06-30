c722916a287f480b036e2f12a380c9c498752f3b
AnkitShrivastava27 <161717274+AnkitShrivastava27@users.noreply.github.com>
2025-06-30 20:39:40 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+echo "Starting Streamlit app..." > /home/site/logs/startup.log
+
+# Start the Streamlit app and log its output
+streamlit run app.py --server.port 8000 --server.address 0.0.0.0 >> /home/site/logs/streamlit.log 2>&1
 
 chmod +x startup.sh
+
